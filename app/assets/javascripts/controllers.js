0b99ce7b1f42b88955f5e1159f62b8c3cc56e6f6
Jacob Burkhart <jacobdouglasburkhart@gmail.com>
2015-02-27 12:11:05 -0600

    updates

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -2,17 +2,23 @@ angular.module('app.controllers', [])
 .controller('homeCtrl', function($scope, $http) {
 
 	var gameCollection = [];
-
-	$http.get('http://www.boardgamegeek.com/xmlapi/search?search=$scope.gameSearcg&exact=1')
-		.success(function(response) {
-
-			gameCollection = [];
-			gameCollection = response.results;
-			// $scope.changeArray = _.sortBy($scope.stateList, function(element) {
-			// 	return element.title.toLowerCase();
-			// });
-		})
-		.error(function(err) {
-			console.log(err);
-		});
+	$scope.searchParam = '';
+	$scope.searchSubmit=function(searchParam){
+		console.log("click");
+		// $http.get('http://www.boardgamegeek.com/xmlapi/search?search='+searchParam+'&exact=1')
+		$http.get('http://www.boardgamegeek.com/xmlapi/search?search='+searchParam)
+			.success(function(response) {
+				console.log(response);
+				console.log(typeof response);
+				for (var i=0; i<response.length; i++)
+				gameCollection = [];
+				gameCollection = response;
+				// $scope.changeArray = _.sortBy($scope.stateList, function(element) {
+				// 	return element.title.toLowerCase();
+				// });
+			})
+			.error(function(err) {
+				console.log(err);
+			});
+		};
 });
