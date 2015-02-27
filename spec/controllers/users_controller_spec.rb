3a56ed4f9eedb9b44518cffba23d0678758c3043
Jmingus <jmingus@austin.rr.com>
2015-02-27 12:15:00 -0600

    Added angular-xml and one test and validations for collections

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  let(:valid_attributes) {
+    skip("Add a hash of attributes valid for your model")
+
+  }
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new User" do
+        expect {
+          post :create, {:user => valid_attributes}, valid_session
+        }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
